806fa32bd16f4afe25aab9513adb991897e67838
Lutfi Altin <lutfi.altin@gmail.com>
2018-11-24 17:13:18 +0100

    Update match.js

--- models/match.js
+++ models/match.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var matchSchema = new mongoose.Schema({
 	id: String,
 	game_name: String,
-	players: String, //json
+	players: [String], //json
 	state: String, //json
 	active_player: Number,
 	game_ended: Boolean 
