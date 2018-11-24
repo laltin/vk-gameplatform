67f2b8f18657d1c157e10ef17eaf432abc8506d3
Lutfi Altin <lutfi.altin@gmail.com>
2018-11-24 18:11:20 +0100

    Update intents.js

--- intents.js
+++ intents.js
@@ -4,6 +4,7 @@ exports.getUserVKName = async function(user_id) {
     var url = "https://api.vk.com/method/users.get?user_id=517947866&v=5.52&access_token=3c1948a499e060bb785162495c2e9fcac510e77497387215c6c031dd41e7a1f04cd1675af66c4de954e92";
 
     var response = await fetch(url);
+    console.log(response);
     var result = await response.json();
 
     return result.response;
