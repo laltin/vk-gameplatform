f45f88b33eee9a1dfc87b013cc54eb8fa0f92139
Lutfi Altin <lutfi.altin@gmail.com>
2018-11-24 12:20:01 +0100

    fix aysnc

--- intents.js
+++ intents.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-exports.checkStartIntent = function(text) {
+exports.checkStartIntent = async function(text) {
     var url = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9497d07e-9ba9-461b-b091-6479652bec4d?subscription-key=789f5de87f9d4ada92e0b393c7d579c0&timezoneOffset=-360&q=' + encodeURIComponent(text);
 
     var response = await fetch(url);
